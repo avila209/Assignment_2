1c6569d1ab9f334c295477510043ed4f28984768
Luis <luis.avila@sjsu.edu>
2019-04-16 19:38:24 -0700

    Setup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    ifstream file ("sample-jobs.dat");
+
+    if(file.is_open())
+    {
+        cout << "file opened correctly" << endl;
+    }
+    else{
+        cout << "Failed" << endl;
+    }
+
+    int data[100][3];
+
+    while(!file.eof())
+    {
+        for(int i = 0; i < 100; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                file >> data[i][j];
+                cout << data[i][j];
+            }
+            cout << endl;
+        }
+    }
+
+    file.close();
     return 0;
 }
